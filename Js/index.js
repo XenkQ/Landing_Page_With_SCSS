2953005441122f6013174f8dc3cd7dca82aec049
XenkQ <77151114+XenkQ@users.noreply.github.com>
2024-02-03 14:03:23 +0100

    Updating nav flexibility
    Updating nav scaling, changing js script responsible for body overflow on nav phone version

--- Js/index.js
+++ Js/index.js
@@ -1,3 +1,4 @@
+const MOBILE_DISPLAY = 700;
 var delay = .5;
 
 function toogleNavBar()
@@ -22,12 +23,13 @@ function toogleNavBar()
 function navBarIn(navElement, body, navMoreButtonElement) {
     navElement.style.animation = "slideIn " + delay + "s ease-in";
     navElement.style.display = "flex";
-    body.style.overflow = "hidden";
     navMoreButtonElement.innerHTML = "<path d='M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z'/>";
+    if(window.innerWidth <= MOBILE_DISPLAY)
+        body.style.overflow = "hidden";
 }
 
 function navBarOut(navElement, body, navMoreButtonElement) {
     navElement.style.animation = "slideOut " + delay + "s ease-out";
-    body.style.overflow = "auto";
     navMoreButtonElement.innerHTML = "<path d='M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3'/>";
+    body.style.overflow = "auto";
 }
